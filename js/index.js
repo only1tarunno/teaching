d99a66b13b7f1350e87b712ba0ca5f4ba1571b1b
only1tarunno <tarunno281@gmail.com>
2024-07-10 11:12:44 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(".testimonialSlider").slick({
+  slidesToShow: 2,
+  dots: true,
+  arrows: true,
+  prevArrow: $(".prev"),
+  nextArrow: $(".next"),
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
